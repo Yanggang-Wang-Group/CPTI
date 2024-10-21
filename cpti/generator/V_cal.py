b648f63ac40f1794553c261fa292d6d48f5ece3e
lemming7519 <lemming7519@126.com>
2024-10-21 15:28:57 +0800

    V-test

--- cpti/generator/V_cal.py
+++ cpti/generator/V_cal.py
@@ -77,14 +77,23 @@ def calele(atomnum, zval,element,index):
 	elem_std=[]
 	ele_std=0
 	ele_bdr=0
-	j=0
+	i,j=0,0
 	num_elem=np.zeros(len(element))
-	for i in range(0,len(atomnum)):
+	while i < len(atomnum):
 		if atomnum[i] <= index[element[j]]:
-			num_elem[j]+=1
+			num_elem[j] += 1
+			i += 1
 		else:
-			j+=1
-			num_elem[j]+=1
+			j += 1
+			i -= 1
+
+	# for i in range(0,len(atomnum)):
+	# 	if atomnum[i] <= index[element[j]]:
+	# 		num_elem[j]+=1
+	# 	else:
+	# 		j+=1
+	# 		num_elem[j]+=1
+
 	print(num_elem)
 	for i in range(0,len(element)):
 		ele_std=ele_std+num_elem[i]*int(float(zval[element[i]]))
